269bb9f428570e7c23abdadf323ef5f8334f42ac
Manav Patel <manav.patel@mail.utoronto.ca>
2020-05-30 01:33:19 -0400

    added bugged version

--- program.py
+++ program.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main(): 
-    #userSummonerName = input('Enter your summoner name: ')
-    #region = input('Enter your region (na, euw, eune, etc...): ')
-    
-    URL = 'https://na.op.gg/summoner/userName=arcaras'
+
+def infoScraper(ign):
+    print ("asd")
+        
+    URL = 'https://na.op.gg/summoner/userName='+ign
     page = requests.get(URL)
     soup = BeautifulSoup(page.text, 'html.parser')
     
@@ -14,6 +14,26 @@ def main():
 
     print(roughRank[23:29]) 
     print(roughLP[33:36])
+
+    return [roughRank, roughLP]
+
+def infoToNum(rank, lp):
+    return 1
+
+#test input: arcaras,clanceejr,100 Tenacity,IllIlIlIl,BladeGO,jojopyun 15,V1per1,Neøø,elise player,DISCORD TARZANED
+def main(): 
+    userSummonerNames = input('Enter 10 summoner names (summoners have to be ranked) separated by commas (no spaces required after commas): ')
+    #region = input('Enter your region (na, euw, eune, etc...): ')
+    splitNames = (str(userSummonerNames)).split(',')
+    
+    if (len(splitNames) == 10):
+        playerDict = {}
+        for name in splitNames:
+            print(infoScraper(name))
+            playerDict[name] = 0
+        
+    else:
+        print("Invalid number of names entered") 
 
 
 #Runs the main function
